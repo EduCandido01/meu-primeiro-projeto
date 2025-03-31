092d2fadc14b537fa1b784b39a31aa7b4b583954
EduCandido01 <c9924932@gmail.com>
2025-03-31 09:05:02 -0300

    primeiro1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class program
+{
+    public static void Main()
+    {
+        double numero1, numero2;
+
+        //WriteLine Escreve a infomação e depois pula 1 linha 
+
+        //White Escreve a informação na tela sem pular linha
+        Console.WriteLine("Entre com o número 1: ");
+
+        //console.readline() - lê o que o usúario digitou 
+
+
+        //captura a informação do usúario e guarda dentro da variavel informacao
+        string informacao = Console.ReadLine();
+
+        //converte o valor da variável informação para double
+
+        //e depois salva o valor dentro da variável numero1
+        numero1 = Convert.ToDouble(informacao);
+
+        Console.WriteLine("Entre o numero 2");
+
+
+        //converte imediataente o valor digitado pelo usúario para double
+        //e depois salva na variável numero2
+        numero2 = Convert.ToDouble(Console.ReadLine());
+
+        double subtracao = numero1 - numero2;
+        double mult = numero1 * numero2;
+        double div = numero1 / numero2;
+        double soma = numero1 + numero2;
+
+        Console.WriteLine($"soma: {soma}");
+
+        Console.WriteLine("subtração: {0}", subtracao);
+
+        Console.WriteLine($"divisao: {div}");
+
+        Console.Write($"multiplicacao: {mult}");
+
+    }
+}
+
+
